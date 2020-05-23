edbbacbd31fd9379612e7dfb093003441a89e91b
Hannah Thorpe <hthorpe@protonmail.com>
2020-05-23 16:09:46 -0400

    knex changed

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
-    || 'postgresql://spanishcapstone@localhost/spaced-repetition',
+  DATABASE_URL: process.env.DATABASE_URL
+    || 'postgres://wxrkgmdzzbtpcv:7aa5ea796ff47aae7e6dad237b95ab84e8f08d00c0613b0e9ca4f530cdfe28c3@ec2-54-81-37-115.compute-1.amazonaws.com:5432/d47cvbs7k2jipf',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
